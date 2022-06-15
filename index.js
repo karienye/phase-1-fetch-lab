7a0a5e3c403cdc3376c9de25990faa309412738a
leah karienye <karienyeleah@gmail.com>
2022-06-15 15:44:13 +0300

    returning your fetch

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function (fetchBooks) { 
+  console.log(fetchBooks);
+  return fetchBooks.json();
+  })
+  .then (function(renderBooks){
+    console.log(renderBooks);
+  })
 }
+/*fetch("http://api.open-notify.org/astros.json")
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });*/
 
 function renderBooks(books) {
   const main = document.querySelector('main');
